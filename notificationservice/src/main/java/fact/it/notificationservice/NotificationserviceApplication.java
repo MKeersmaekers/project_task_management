bc6581380b63533b511fc235751bce014f2a1245
MKeersmaekers <r0889348@student.thomasmore.be>
2023-11-15 11:31:36 +0100

    all services fixed

--- notificationservice/src/main/java/fact/it/notificationservice/NotificationserviceApplication.java
+++ notificationservice/src/main/java/fact/it/notificationservice/NotificationserviceApplication.java
@@ -2,8 +2,9 @@ package fact.it.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class NotificationserviceApplication {
 
 	public static void main(String[] args) {
