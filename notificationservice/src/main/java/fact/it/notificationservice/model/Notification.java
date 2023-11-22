2e013a2a6aa0bd8c580eb2af461b62485a447bea
MKeersmaekers <r0889348@student.thomasmore.be>
2023-11-22 12:51:57 +0100

    notif update

--- notificationservice/src/main/java/fact/it/notificationservice/model/Notification.java
+++ notificationservice/src/main/java/fact/it/notificationservice/model/Notification.java
@@ -1,10 +1,10 @@
 package fact.it.notificationservice.model;
 
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "notifications")
